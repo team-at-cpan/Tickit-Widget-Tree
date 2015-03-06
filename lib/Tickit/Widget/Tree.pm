4ffd07d5dd7800cfa8e11100c8f31a32c1183ee3
Tom Molesworth <tom@perlsite.co.uk>
2015-03-06 15:25:17 +0000

    no guarantee Scalar::Util is loaded, I guess
    git-svn-id: http://entitymodel.com/repo/trunk/Tickit/Tickit-Widget-Tree@965 c09ddf91-2ef0-45e8-8c68-47ccf8e3121d

--- lib/Tickit/Widget/Tree.pm
+++ lib/Tickit/Widget/Tree.pm
@@ -252,7 +252,7 @@ sub new {
 		$add = sub {
 			my ($parent, $item) = @_;
 			if(my $ref = ref $item) {
-				if(blessed $item) {
+				if(Scalar::Utils::blessed($item)) {
 					# If we already have an adapter, apply it now - it'll autopopulate immediately
 					if($item->isa('Adapter::Async::OrderedList')) {
 						$self->adapter_for_node($parent => $item);
