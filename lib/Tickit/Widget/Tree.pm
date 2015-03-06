6cae71b115c34694dc70a7aee0a3c0e57aa60e91
Tom Molesworth <tom@perlsite.co.uk>
2015-03-06 15:25:13 +0000

    Update copyright
    git-svn-id: http://entitymodel.com/repo/trunk/Tickit/Tickit-Widget-Tree@959 c09ddf91-2ef0-45e8-8c68-47ccf8e3121d

--- lib/Tickit/Widget/Tree.pm
+++ lib/Tickit/Widget/Tree.pm
@@ -746,5 +746,5 @@ Tom Molesworth <cpan@perlsite.co.uk>
 
 =head1 LICENSE
 
-Copyright Tom Molesworth 2011-2013. Licensed under the same terms as Perl itself.
+Copyright Tom Molesworth 2011-2015. Licensed under the same terms as Perl itself.
 
