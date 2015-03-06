429f41919f821cfcb135bbe02a127cb3099e01fd
Tom Molesworth <tom@perlsite.co.uk>
2015-03-06 15:25:39 +0000

    Use subclass
    git-svn-id: http://entitymodel.com/repo/trunk/Tickit/Tickit-Widget-Tree@1010 c09ddf91-2ef0-45e8-8c68-47ccf8e3121d

--- examples/adapter.pl
+++ examples/adapter.pl
@@ -4,22 +4,22 @@ use warnings;
 use Tickit::Async;
 use Tickit::Widget::Tree;
 
-my $tree = Tree::DAG_Node->random_network({ max_depth => 20, min_depth => 6, max_children => 3, max_node_count => 5000 }); # Tree::DAG_Node->new;
+my $tree = Tickit::Widget::Tree::Node->random_network({ max_depth => 20, min_depth => 6, max_children => 3, max_node_count => 5000 }); # Tickit::Widget::Tree::Node->new;
 $tree->name('Root');
 $tree->walk_down({ callback => sub { shift->attributes->{open} = 0 } });
 $tree->attributes->{open} = 1;
 my $adapter;
 my $n;
-$tree->add_daughter($n = Tree::DAG_Node->new({name => 'one'}));
-$n->add_daughter(Tree::DAG_Node->new({name => 'one.one'}));
-$tree->add_daughter($n = Tree::DAG_Node->new({name => 'two'}));
-$n->add_daughter(Tree::DAG_Node->new({name => 'two.one'}));
-$n->add_daughter(Tree::DAG_Node->new({name => 'two.two'}));
-$n->add_daughter(Tree::DAG_Node->new({name => 'two.three'}));
+$tree->add_daughter($n = Tickit::Widget::Tree::Node->new({name => 'one'}));
+$n->add_daughter(Tickit::Widget::Tree::Node->new({name => 'one.one'}));
+$tree->add_daughter($n = Tickit::Widget::Tree::Node->new({name => 'two'}));
+$n->add_daughter(Tickit::Widget::Tree::Node->new({name => 'two.one'}));
+$n->add_daughter(Tickit::Widget::Tree::Node->new({name => 'two.two'}));
+$n->add_daughter(Tickit::Widget::Tree::Node->new({name => 'two.three'}));
 $n->attributes->{open} = 1;
-$tree->add_daughter($n = Tree::DAG_Node->new({name => 'three'}));
-$n->add_daughter(Tree::DAG_Node->new({name => "three.$_"})) for qw(one two three four five six seven);
-$tree->add_daughter(Tree::DAG_Node->new({name => 'four'}));
+$tree->add_daughter($n = Tickit::Widget::Tree::Node->new({name => 'three'}));
+$n->add_daughter(Tickit::Widget::Tree::Node->new({name => "three.$_"})) for qw(one two three four five six seven);
+$tree->add_daughter(Tickit::Widget::Tree::Node->new({name => 'four'}));
 # warn "$_\n" for @{$tree->tree2string};
 my $w = Tickit::Widget::Tree->new(root => $tree);
 my $tickit = Tickit::Async->new(
@@ -29,7 +29,7 @@ my $tickit = Tickit::Async->new(
 	}
 );
 {
-	$tree->add_daughter(my $ab = Tree::DAG_Node->new({name => 'adapterized'}));
+	$tree->add_daughter(my $ab = Tickit::Widget::Tree::Node->new({name => 'adapterized'}));
 	my $adapter = $w->adapter_for_node($ab);
 	$adapter->push([int 17*rand]) for 1..5;
 	my $code;
